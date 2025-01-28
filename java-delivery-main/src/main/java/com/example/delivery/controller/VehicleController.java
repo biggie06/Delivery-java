ef490b0131c80324dc2d7a0eaa875adc18826261
JOSEPH AGWUNGWU <joseph.agwungwu@binghamuni.edu.ng>
2025-01-28 15:52:40 +0100

    Delivery

--- java-delivery-main/src/main/java/com/example/delivery/controller/VehicleController.java
+++ java-delivery-main/src/main/java/com/example/delivery/controller/VehicleController.java
@@ -1,27 +1,45 @@
-package com.example.delivery.controller;
+package com.mesh.delivery.controller;
+
+import com.mesh.delivery.Entity.Item;
+import com.mesh.delivery.service.ItemService;
+import com.mesh.delivery.service.VehicleServiceImplemenntation;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.*;
+
+import com.mesh.delivery.Entity.Vehicle;
+import com.mesh.delivery.service.VehicleService;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.example.delivery.entities.Vehicle;
-import com.example.delivery.services.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 @RequestMapping("/api/v1/vehicle")
 public class VehicleController {
 
+    @Autowired
     private VehicleService vehicleService;
-    
-    public VehicleController(VehicleService vehicleService) {
-        this.vehicleService = vehicleService;
-    }
+    @Autowired
+    private VehicleServiceImplemenntation vehicleServiceImplemenntation;
 
     @PostMapping("/create-vehicle")
-    ResponseEntity<Vehicle> createVehicle(@RequestBody Vehicle vehicle) {
+    public ResponseEntity<Vehicle> createVehicle(@RequestBody Vehicle vehicle) {
         vehicleService.createVehicle(vehicle);
         return ResponseEntity.ok(vehicle);
     }
+
+    @GetMapping("/get-vehicle/{plateNumber}")
+    ResponseEntity<Vehicle> getVehicleByplateNumber(@PathVariable String plateNumber) {
+        Vehicle vehicle = vehicleService.getVehicleByPlateNumber(plateNumber);
+        return  ResponseEntity.ok(vehicle);
+    }
+    @PostMapping("add-item-to-vehicle/{plateNumber}/item/{itemId}")
+    ResponseEntity<Vehicle> addItemToVehicle(@PathVariable String plateNumber, @PathVariable Long itemId){
+        Item item = ItemService.getItemById(itemId);
+        Vehicle vehicle = vehicleService.getVehicleByPlateNumber(plateNumber);
+        vehicle.getItems().add(item);
+        vehicleService.createVehicle(vehicle);
+        return ResponseEntity.ok(vehicle);
+    }
+
 }
